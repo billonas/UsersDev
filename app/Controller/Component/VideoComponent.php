1d56d119409793b7073067c4b5c2dfb677d4b7c3
Panagiotis <panagiotis@panagiotis-P35-DS4.(none)>
2012-06-02 22:05:47 +0300

    VideoComponent

--- app/Controller/Component/VideoComponent.php
+++ app/Controller/Component/VideoComponent.php
@@ -6,34 +6,35 @@
  */
 //http://www.ehow.com/how_6938294_upload-php-video-files.html
 class VideoComponent extends Component {
- 
-    public function uploadVideo($file, $folder){
-    //This handles the maximum size for the video file in kbs
 
-    define ("MAX_SIZE","500000");
-    //This function reads the extension of the file to ensure that it is an video file
-    function getExtension($str) {
+
+    public function uploadVideo($file, $folder){
+function getExtension($str) {
         $i = strrpos($str,".");
         if (!$i) { return ""; }
          $l = strlen($str) - $i;
         $ext = substr($str,$i+1,$l);
         return $ext;
-    }
+    } 
+    //This handles the maximum size for the video file in kbs
+
+    define ("MAX_SIZE","500000");
+    //This function reads the extension of the file to ensure that it is an video file
     
     //This variable handles an error and won't upload the file if there is a problem with it
     $errors=0;
     //checks if the form has been submitted
     //if(isset($_POST['Submit']))
-    if(empty($file['video']))   echo "oxi re paidi";
-    if(isset($file['video']))
+    if(empty($file))   $errors = 1; //no file uploaded(or file biger thanupload_max_file(php.ini)
+    if(isset($file))
     {
     //reads the name of the file the user submitted for uploading
-    $video=$file['video']['name'];
+    $video=$file['name'];
     //if it is not empty
     if ($video){
     //get the original name of the file from the clients machine
 
-    $video_filename = stripslashes($file['video']['name']);
+    $video_filename = stripslashes($file['name']);
 
     $video_extension = getExtension($video_filename);
 
@@ -45,9 +46,9 @@ if (($video_extension != "mpeg") && ($video_extension != "avi") && ($video_exten
 
 {
 
-echo '<h1>Unknown extension!</h1>';
+$errors = 2;//'<h1>Unknown extension!</h1>';
 
-$errors=1;
+
 
 }
 
@@ -57,7 +58,7 @@ else
 
 //get the size of the video
 
-$size=filesize($file['video']['tmp_name']);
+$size=filesize($file['tmp_name']);
 
 //compare the size with the maxim size we defined and print error if bigger
 
@@ -65,9 +66,8 @@ if ($size > MAX_SIZE*1024)
 
 {
 
-echo '<h1>You have exceeded the size limit!</h1>';
+$errors = 3;//echo '<h1>You have exceeded the size limit!</h1>';
 
-$errors=1;
 
 }
 
@@ -80,26 +80,38 @@ $video_name=time().'.'.$video_extension;
 $newname= $folder.$video_name;
 
 //verify that the video has been loaded
-$copied = copy($file['video']['tmp_name'], $newname);
+$copied = copy($file['tmp_name'], $newname);
 
 if (!$copied)
 
 {
 
-echo '<h1>Copy unsuccessful!</h1>';
+$errors = 4;//echo '<h1>Copy unsuccessful!</h1>';
 
-$errors=1;
 
 }}}}
 
 //If no errors registered, print the success message
-if(isset($file['video']) && !$errors)
 
-{
-
-echo "<h1>File Uploaded Successfully! Try again!</h1>";
+$uploaded = array('error' => $errors, 'path' => $newname);//echo "<h1>File Uploaded Successfully!</h1>";
+return $uploaded;
 
 }
-}
+
+ public function mvSubVideo($report, $name, $dir, $ext = ""){
+        if(empty($name))
+                return 1;
+         $newNameId = $report->id;  //to id ths eggrafhs pou molis prostethhke
+	 $tok = strtok (  $name, "." ); //briskw thn katalhksh tou video
+         while(($tok1 = strtok(".")) !== false){
+		$tok = $tok1;      		
+	}
+	//dinw sto video gia onoma to id ths eggrafhs(me thn katallhlh katalhksh) kai to metaferw tautoxrona ston fakelo
+        //webroot/video/$dir
+	$newName = "$dir/$newNameId$ext.$tok";  
+	rename("$name", "video/$newName");
+        if(!$report->saveField("video", $newName)) return 0;
+        return 1;
+    }
 }
 ?>
