03cd148e5beb9db1f27c038afdf516ea6359952b
nikos <>
2012-07-05 00:51:40 +0300

    Allages ston news controller.Dimiourgia methodou view.

--- app/Controller/NewsController.php
+++ app/Controller/NewsController.php
@@ -7,17 +7,35 @@
  */
 class NewsController extends AppController {
     public $name = 'News';
-    public $helpers = array('Html', 'Form');
+    public $helpers = array('Html', 'Form','Session');
     var $uses = array('New', 'News');
 
     
     public function show() {
+        if((!$this->Session->check('UserUsername')) || 
+                    (strcmp($this->Session->read('UserType'), 'hyperanalyst')))
+        {
+            $this->redirect(array('controller'=>'pages', 'action'=>'display'));  
+        }
+        else
+        {
+        $this->set('news', $this->New->find('all'));
+        }
+    }
+    
+    public function view() {
         $this->set('news', $this->New->find('all'));
         
     }
     
     public function create() {
-        if ($this->request->is('post')) {
+        if((!$this->Session->check('UserUsername')) || 
+                    (strcmp($this->Session->read('UserType'), 'hyperanalyst')))
+        {
+            $this->redirect(array('controller'=>'pages', 'action'=>'display'));  
+        }
+        else
+        {
             if ($this->New->save($this->request->data)) {
                 $this->Session->setFlash('Your new has been saved.');
                 $this->redirect(array('action' => 'show'));
@@ -28,27 +46,43 @@ class NewsController extends AppController {
     }
     
     public function edit($id = null) {
-    $this->New->id = $id;
-    if ($this->request->is('get')) {
-        $this->request->data = $this->New->read();
-    } else {
-        if ($this->New->save($this->request->data)) {
-            $this->Session->setFlash('Your new has been updated.');
-            $this->redirect(array('action' => 'show'));
-        } else {
-            $this->Session->setFlash('Unable to update your new.');
+        if((!$this->Session->check('UserUsername')) || 
+                    (strcmp($this->Session->read('UserType'), 'hyperanalyst')))
+        {
+            $this->redirect(array('controller'=>'pages', 'action'=>'display'));  
         }
-    }
+        else
+        {
+        $this->New->id = $id;
+        if ($this->request->is('get')) {
+            $this->request->data = $this->New->read();
+        } else {
+            if ($this->New->save($this->request->data)) {
+                $this->Session->setFlash('Your new has been updated.');
+                $this->redirect(array('action' => 'show'));
+            } else {
+                $this->Session->setFlash('Unable to update your new.');
+            }
+        }
+        }
     }
     
     public function delete($id) {
-    if ($this->request->is('get')) {
-        throw new MethodNotAllowedException();
-    }
-    if ($this->New->delete($id)) {
-        $this->Session->setFlash('The new with id: ' . $id . ' has been deleted.');
-        $this->redirect(array('action' => 'show'));
-    }
+        if((!$this->Session->check('UserUsername')) || 
+                    (strcmp($this->Session->read('UserType'), 'hyperanalyst')))
+        {
+            $this->redirect(array('controller'=>'pages', 'action'=>'display'));  
+        }
+        else
+        {
+        if ($this->request->is('get')) {
+            throw new MethodNotAllowedException();
+        }
+        if ($this->New->delete($id)) {
+            $this->Session->setFlash('The new with id: ' . $id . ' has been deleted.');
+            $this->redirect(array('action' => 'show'));
+        }
+        }
     }
 }
 ?>
